4c8ef6f9b9cb11f3e6e7a1cbfcd70683cf29952e
Vano <iw.lasgin2011@gmail.com>
2020-03-02 23:17:58 +0300

    analyze points

--- Point.py
+++ Point.py
@@ -5,6 +5,12 @@ class Point:
         self.y = y
         self.value = value
         self.time = time
+        
+    def get_value(self):
+        return self.value
+    
+    def get_time(self):
+        return self.time
 
 def get_point_from_json(json_object):
         return Point(json_object['x'], json_object['y'], json_object['value'], json_object['time'])
---
+++ PollutionAnalyzer.py
@@ -0,0 +1,34 @@
+import Point
+
+TIME_STEP = 1000 * 60
+
+def analyze(points) :
+    
+    startTime = points[0]
+    endTime = points[len(points) - 1]
+    duration = endTime - startTime
+    
+    if(len(points) == 1) :
+        return points[0].get_value(), duration
+    
+    totalValue = 0
+
+    for i in range(1, len(points)):
+        prevIndex = i - 1
+        prevPoint = points[prevIndex]
+        currentPoint = points[i]
+        
+        totalValue += calculate_value(prevPoint, currentPoint)
+        
+    return totalValue,duration
+
+def calculate_value(prevPoint, currentPoint) :
+    prevTime = prevPoint.get_time()
+    currentTime =  currentPoint.get_time()
+
+    timeDiff = (currentTime - prevTime)/ TIME_STEP
+    print("timeDiff = ", timeDiff)
+    
+    value = currentPoint.get_value() * timeDiff
+    
+    return value
